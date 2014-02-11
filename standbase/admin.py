25ae7e4ea538341a4414eb5a1c995642745da51c
Alper Çugun <github@alper.nl>
2014-02-11 16:37:12 +0100

    hooked up catch method

--- standbase/admin.py
+++ standbase/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from standbase.models import *
 
 class StandSessionAdmin(admin.ModelAdmin):
-    list_display = ('lat', 'lon', 'message', 'vendorid')
+    list_display = ('lat', 'lon', 'message', 'vendorid', 'venueid')
 
 admin.site.register(StandSession, StandSessionAdmin)
--- standbase/urls.py
+++ standbase/urls.py
@@ -4,7 +4,7 @@ from standbase import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    # url(r'^signup$', views.signup_page, name='signup-page'),
+    url(r'^catch$', views.catch, name='catch'),
     # url(r'^signup_success$', views.signup_success, name='signup-success'),
 
     # url(r'^sms_catcher$', views.sms_catcher, name='sms-catcher'),
