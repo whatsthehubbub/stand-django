be5f9a44a0e25b1dba26646dc5db14ea4a8c600b
Alper Çugun <github@alper.nl>
2014-02-11 19:18:56 +0100

    added datecraeted field

--- standbase/admin.py
+++ standbase/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from standbase.models import *
 
 class StandSessionAdmin(admin.ModelAdmin):
-    list_display = ('lat', 'lon', 'message', 'vendorid', 'venueid')
+    list_display = ('datecreated', 'lat', 'lon', 'message', 'vendorid', 'venueid')
 
 admin.site.register(StandSession, StandSessionAdmin)
