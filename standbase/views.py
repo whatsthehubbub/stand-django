66dd0d4de87c8ff07b56521c7b39c9a73438aba4
Alper Çugun <github@alper.nl>
2014-03-04 18:33:43 +0100

    excluded sessions for hidden topics from frontpage

--- standbase/views.py
+++ standbase/views.py
@@ -19,9 +19,10 @@ logger = logging.getLogger('testlogger')
 
 
 def index(request):
+    # Todo make a custom manager on StandSession to filter these
     return render(request, 'standbase/index.html', {
-        'active_sessions': StandSession.objects.filter(datefinished=None).filter(datelive__gt=timezone.now()-datetime.timedelta(seconds=300)).order_by('-datecreated'),
-        'completed_sessions': StandSession.objects.exclude(datefinished=None).order_by('-datefinished')
+        'active_sessions': StandSession.objects.filter(topic__public=True).filter(datefinished=None).filter(datelive__gt=timezone.now()-datetime.timedelta(seconds=300)).order_by('-datecreated'),
+        'completed_sessions': StandSession.objects.filter(topic__public=True).exclude(datefinished=None).order_by('-datefinished')
     })
 
 def session(request, sessionid):
